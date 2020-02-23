a98984fe274517c22a69ebc5ca62c203ab16e195
NagShankar <nag.shankar6@gmail.com>
2020-02-23 10:51:41 +0530

    updated

--- Public/scripts/app.js
+++ Public/scripts/app.js
@@ -10,9 +10,9 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
 
 function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
 
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
 function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
 
@@ -32,7 +32,7 @@ function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || func
 //    e.preventDefault();
 //    console.log('Form submitted!');
 //    
-//    const option=e.target.elements.option.value;//fetch value give in input name 'option' ans store it in variable option
+//    const option=e.target.elements.option.value;//fetch value give in input name 'option' and store it in variable option
 //    if(option){
 //        app2.options.push(option); //push the value into 
 //        e.target.elements.option.value='';//emptying the input box
@@ -98,16 +98,81 @@ var IndecisionApp =
 function (_React$Component) {
   _inherits(IndecisionApp, _React$Component);
 
-  function IndecisionApp() {
+  function IndecisionApp(props) {
+    var _this;
+
     _classCallCheck(this, IndecisionApp);
 
-    return _possibleConstructorReturn(this, _getPrototypeOf(IndecisionApp).apply(this, arguments));
+    _this = _possibleConstructorReturn(this, _getPrototypeOf(IndecisionApp).call(this, props));
+    _this.handleDeleteOptions = _this.handleDeleteOptions.bind(_assertThisInitialized(_this));
+    _this.selectRandomNum = _this.selectRandomNum.bind(_assertThisInitialized(_this));
+    _this.handleAddOption = _this.handleAddOption.bind(_assertThisInitialized(_this)); //setting dafault state
+
+    _this.state = {
+      //options:['marc', 'alex', 'honda']
+      options: []
+    };
+    {
+      /*now we need to send these options to Options component which has wipeall method which can delete and make the options zero, since button to wipe all doesnt exist in this component, hence sending as props to Options component
+      */
+    }
+    return _this;
   }
 
   _createClass(IndecisionApp, [{
+    key: "handleDeleteOptions",
+    value: function handleDeleteOptions() {
+      this.setState(function () {
+        return {
+          options: []
+        };
+      });
+    }
+  }, {
+    key: "handleAddOption",
+    value: function handleAddOption(option) {
+      //alert(option);
+      //validation
+      if (!option) {
+        //if option doesnt exist
+        return 'Please add something :(';
+      } else if (this.state.options.indexOf(option) > -1) {
+        return 'Option already exist!!!!';
+      } else {
+        this.setState(function (prevState) {
+          //prevState.options.push(option); //we shoundnt do this, cuz we're manipulating prevState directly by pushin new option, push basically alter original array, we shud use concat instead
+          return {
+            //options:prevState.options  //here we directly manipulated prevState and assgining it to Options array, which is actually origianl array, we should use concat instead
+            //using concat now
+            options: prevState.options.concat([option]) //or options:prevState.options.concat(option) - passing non array value, both give same result, and new array is returned, which is basic behaviour of concat to return new array
+
+          };
+        });
+      }
+    }
+  }, {
+    key: "selectRandomNum",
+    value: function selectRandomNum() {
+      var randomNum = Math.floor(Math.random() * this.state.options.length);
+      alert(randomNum + " " + this.state.options[randomNum]);
+    }
+  }, {
     key: "render",
     value: function render() {
-      return React.createElement("div", null, React.createElement(Header, null), React.createElement(Action, null), React.createElement(Options, null), React.createElement(AddOption, null));
+      var titleText = 'Indicision App';
+      var subTitleText = 'Small application using React';
+      return React.createElement("div", null, React.createElement(Header, {
+        title: titleText,
+        subtitle: subTitleText
+      }), React.createElement(Action, {
+        hasOptions: this.state.options.length > 0,
+        sendRandomNum: this.selectRandomNum
+      }), React.createElement(Options, {
+        allOptions: this.state.options,
+        resettingOptions: this.handleDeleteOptions
+      }), React.createElement(AddOption, {
+        addingOption: this.handleAddOption
+      }));
     }
   }]);
 
@@ -131,7 +196,7 @@ function (_React$Component2) {
     value: function render() {
       //es6 may not have any methods defined but in React needs render method to be defined all the time
       //render returns JSX
-      return React.createElement("div", null, React.createElement("h1", null, "Indicision App"), React.createElement("h2", null, "Small application using React"));
+      return React.createElement("div", null, React.createElement("h1", null, this.props.title), React.createElement("h2", null, this.props.subtitle));
     }
   }]);
 
@@ -151,14 +216,26 @@ function (_React$Component3) {
 
   _createClass(Action, [{
     key: "render",
+    //    handlePick(){
+    //    alert('Damn i was clicked!!!');    
+    //    }
+    //     wipeAll(){
+    //        alert('Warning! i\'ll wipe all');
+    //        
+    //    }
     value: function render() {
-      return React.createElement("button", null, "What should i do?");
+      return React.createElement("div", null, React.createElement("button", {
+        onClick:
+        /*this.handlePick*/
+        this.props.sendRandomNum,
+        disabled: !this.props.hasOptions
+      }, "What should i do? Pick random option"));
     }
   }]);
 
   return Action;
 }(React.Component);
-/* nesting component inside Options component */
+/* nesting Option component inside Options component */
 
 
 var Options =
@@ -174,8 +251,26 @@ function (_React$Component4) {
 
   _createClass(Options, [{
     key: "render",
+    //this is efficient way to bind
+    //    constructor(props){
+    //        super(props);
+    //        this.wipeAll=this.wipeAll.bind(this);
+    //        
+    //    }
+    //      wipeAll(){
+    //        //alert('Warning! i\'ll wipe all');
+    //          console.log(this.props.allOptions);
+    //        
+    //    }
     value: function render() {
-      return React.createElement("div", null, React.createElement("p", null, "Here are your options"), React.createElement(Option, null));
+      return React.createElement("div", null, React.createElement("button", {
+        onClick: this.props.resettingOptions
+      }, "Wipe All"), React.createElement("p", null, "Here are your options"), React.createElement("p", null, "Here is the length: ", this.props.allOptions.length), React.createElement("ol", null, this.props.allOptions.map(function (option) {
+        return React.createElement(Option, {
+          key: option,
+          optionText: option
+        });
+      })));
     }
   }]);
 
@@ -187,16 +282,53 @@ var AddOption =
 function (_React$Component5) {
   _inherits(AddOption, _React$Component5);
 
-  function AddOption() {
+  //using constructor here sine we're using props inside addOption method and we need to bind this
+  function AddOption(props) {
+    var _this2;
+
     _classCallCheck(this, AddOption);
 
-    return _possibleConstructorReturn(this, _getPrototypeOf(AddOption).apply(this, arguments));
+    _this2 = _possibleConstructorReturn(this, _getPrototypeOf(AddOption).call(this, props));
+    _this2.addOption = _this2.addOption.bind(_assertThisInitialized(_this2)); //receiving error
+
+    _this2.state = {
+      error: false //by default its undefined, in other words false
+
+    };
+    return _this2;
   }
 
   _createClass(AddOption, [{
+    key: "addOption",
+    value: function addOption(e) {
+      //alert('i will add something');
+      e.preventDefault(); //const option=e.target.elements.option.value;
+
+      var option = e.target.elements.option.value.trim(); //trimming out empty spaces
+      //now vaidation is done inside parent component itself
+      //        if(option){
+      //           //alert(option);
+      //            this.props.addingOption(option);
+      //            e.target.elements.option.value='';//emptying the input box after submitting
+      //        }
+
+      var whatsTheOutput = this.props.addingOption(option);
+      this.setState(function () {
+        return {
+          error: whatsTheOutput
+        };
+      });
+      e.target.elements.option.value = ''; //emptying the input box after submitting
+    }
+  }, {
     key: "render",
     value: function render() {
-      return React.createElement("div", null, React.createElement("button", null, "Add Options"));
+      return React.createElement("div", null, React.createElement("form", {
+        onSubmit: this.addOption
+      }, React.createElement("input", {
+        type: "text",
+        name: "option"
+      }), React.createElement("button", null, "Add Options")), this.state.error && React.createElement("p", null, this.state.error));
     }
   }]);
 
@@ -217,7 +349,7 @@ function (_React$Component6) {
   _createClass(Option, [{
     key: "render",
     value: function render() {
-      return React.createElement("div", null, React.createElement("p", null, "Every single option will go here...."));
+      return React.createElement("li", null, this.props.optionText);
     }
   }]);
 
